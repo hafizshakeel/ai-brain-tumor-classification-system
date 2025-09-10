12a12528670a5d6c1e7cf787eaf9dd75d7e78f93
hafizshakeel <hafizshakeel1997@gmail.com>
2025-09-10 13:24:43 +0500

    chore: Initialize project structure and dependencies

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "brain_tumor_classification"
+AUTHOR_USER_NAME = "Hafiz Shakeel Ahmad"
+SRC_REPO = "brain_tumor_classification"
+AUTHOR_EMAIL = "hafizshakeel1997@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for CV app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+)
